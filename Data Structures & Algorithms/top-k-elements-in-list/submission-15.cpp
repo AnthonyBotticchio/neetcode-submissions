a5d517837801d9fad7e86e1720eeeff758887c65
Anthony Botticchio <151904729+AnthonyBotticchio@users.noreply.github.com>
2026-08-15 02:46:55 -0400

    Add: top-k-elements-in-list - submission-15

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-15.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    std::unordered_map<int, unsigned int> countMap;
+
+   public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        if (nums.size() == 1) {
+            return std::vector<int>{*nums.begin()};
+        }
+
+        for (const auto i : nums) {
+            countMap[i]++;
+        }
+
+        std::vector<std::pair<int, unsigned int>> sorted(countMap.begin(), countMap.end());
+        // Sort values
+        const auto& target_k = std::min<int>(k, sorted.size());
+        std::partial_sort(sorted.begin(), sorted.begin() + target_k, sorted.end(),
+                          [](const auto& l, const auto& r) { return l.second > r.second; });
+        std::vector<int> res(target_k);  // Reserve target_k space
+        std::transform(sorted.begin(), sorted.begin() + target_k, res.begin(),
+                       [](const auto& pair) { return pair.first; });
+
+        return res;
+    }
+};
