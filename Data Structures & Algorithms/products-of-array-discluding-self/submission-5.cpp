4023ddb8eda9b482c6b9f7ea8cebd2bfcc19c51f
Anthony Botticchio <151904729+AnthonyBotticchio@users.noreply.github.com>
2026-08-17 01:01:30 -0400

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        std::vector<int> res(nums.size(), 1);
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            res[i] = nums[i - 1] * res[i - 1];
+        }
+
+        int postFix = 1;
+        for(int i = nums.size() - 1; i >= 0; i--)
+        {
+            res[i] *= postFix;
+            postFix *= nums[i];
+        }
+
+        return res;
+    }
+};
